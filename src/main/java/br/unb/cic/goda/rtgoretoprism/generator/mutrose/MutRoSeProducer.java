9d2cb5aa35dff8c21ffe0afd16fc4d8b0223a37e
Rafael Souza <rafaelsoec@gmail.com>
2021-10-03 19:12:11 -0300

    Atualizando binario

--- src/main/java/br/unb/cic/goda/rtgoretoprism/generator/mutrose/MutRoSeProducer.java
+++ src/main/java/br/unb/cic/goda/rtgoretoprism/generator/mutrose/MutRoSeProducer.java
@@ -8,6 +8,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.json.simple.JSONObject;
@@ -29,28 +30,36 @@ public class MutRoSeProducer {
 
 		try {
 			String dir = "mrs/";
-			String dirOutput = dir + "output/";
+			String dirOutput =  dir + "output/";
 			JSONObject jsonObject = this.updatePathConfigurationFile(configuration, dirOutput);
 
 			// gerar arquivos
-			ManageWriter.generateFile(dir, "model.txt", model);
-		    ManageWriter.generateFile(dir, "configFile.json", jsonObject.toJSONString());
-			ManageWriter.generateFile(dir, "worldKnowledge.xml", worldKnowledge);
-			ManageWriter.generateFile(dir, "configHddl.hddl", hddl);
+			File modelFile = ManageWriter.generateFile(dir, "model.txt", model);
+			File configFile = ManageWriter.generateFile(dir,  "configFile.json", jsonObject.toJSONString());
+			File worldKnowledgeFile = ManageWriter.generateFile(dir,  "worldKnowledge.xml", worldKnowledge);
+			File hddlFile = ManageWriter.generateFile(dir,  "configHddl.hddl", hddl);
+			
+			
 			JSONObject outputConfig = (JSONObject) jsonObject.get("output");
 			String output = (String) outputConfig.get("file_path");
-			ManageWriter.generateFile(output, "");
-
+//			ManageWriter.generateFile(output, "");
+//            File resultsFile = ManageWriter.generateFile(output, "");
+//			File resultsFile = File.createTempFile(output, null);
 			
-			StringBuilder command = new StringBuilder().append("./").append(dir).append("MRSDecomposer ").append(dir)
-					.append("configHddl.hddl ").append(dir).append("model.txt ").append(dir).append("configFile.json ")
-					.append(dir).append("worldKnowledge.xml ");
+			StringBuilder command = new StringBuilder()
+					.append("chmod -x ./").append(dir).append("MRSDecomposer").append(" ")
+					.append(hddlFile.getAbsolutePath()).append(" ")
+					.append(modelFile.getAbsolutePath()).append(" ")
+					.append(configFile.getAbsolutePath()).append(" ")
+					.append(worldKnowledgeFile.getAbsolutePath());
 
 			Process proc = Runtime.getRuntime().exec(command.toString());
 			LOGGER.info(proc.getInputStream().toString());
 			LOGGER.info(proc.getOutputStream().toString());
+			
+//			invokeAndGetResult(command.toString(), resultsFile.getAbsolutePath() + ".out");
 
-			ManageWriter.toCompact(output, "src/main/webapp/mrs.zip");
+			ManageWriter.toCompact(new File(output).getAbsolutePath(), "src/main/webapp/mrs.zip");
 		} catch (Exception error) {
 			throw new RuntimeException(error);
 
@@ -79,4 +88,50 @@ public class MutRoSeProducer {
 			throw new RuntimeException(e);
 		}
 	}
+
+    private String invokeAndGetResult(String commandLine, String resultsPath) throws IOException {
+        LOGGER.fine(commandLine);
+        Process program = Runtime.getRuntime().exec(commandLine);
+        int exitCode = 0;
+        try {
+            exitCode = program.waitFor();
+        } catch (InterruptedException e) {
+        	LOGGER.severe("Error invoking param with command:" + commandLine);
+        	
+            LOGGER.severe("Exit code: " + exitCode);
+            LOGGER.log(Level.SEVERE, e.toString(), e);
+        }
+        
+        logExecResults(program);
+        
+        List<String> lines = Files.readAllLines(Paths.get(resultsPath), Charset.forName("UTF-8"));
+        // Formula
+        return lines.get(lines.size() - 1);
+    }
+    
+    private void logExecResults(Process proc) throws IOException {
+        BufferedReader stdInput = new BufferedReader(new 
+       	     InputStreamReader(proc.getInputStream()));
+       
+       BufferedReader stdError = new BufferedReader(new 
+       	     InputStreamReader(proc.getErrorStream()));
+       
+       String s = null;
+       
+       if(stdInput.ready()) {
+	       // Read the output from the command
+	       System.out.println("Here is the standard output of the command:");
+	       while ((s = stdInput.readLine()) != null) {
+	           System.out.println(s);
+	       }
+       }
+
+       if(stdError.ready()) {
+           // Read any errors from the attempted command
+           System.err.println("Here is the standard error of the command (if any):");
+           while ((s = stdError.readLine()) != null) {
+               System.err.println(s);
+           }
+       }
+    }
 }
