7aadc9c9e971832cb63fbd84d52fe18099d6ebd6
Rafael Souza <rafaelsoec@gmail.com>
2021-10-19 18:12:04 -0300

    Teste com o mutrose bin

--- src/main/java/br/unb/cic/goda/rtgoretoprism/generator/analiser/SintaticAnaliser.java
+++ src/main/java/br/unb/cic/goda/rtgoretoprism/generator/analiser/SintaticAnaliser.java
@@ -14,25 +14,26 @@ public class SintaticAnaliser {
 	private static final Logger LOGGER = Logger.getLogger(SintaticAnaliser.class.getName());
 
 	public static String recoverLogsError() {
-		String dir = "/sintaticAnaliser/";
+		String dir = "sintaticAnaliser/";
 
 		try {
 
-			StringBuilder command = new StringBuilder().append("./").append(dir)
+			StringBuilder command = new StringBuilder().append("chmod +x ./").append(dir)
 					.append("goal-model-syntactic-analyzer-linux");
 
 			Process proc = Runtime.getRuntime().exec(command.toString());
 			LOGGER.info(proc.getInputStream().toString());
 			LOGGER.info(proc.getOutputStream().toString());
-
-			List<String> lines = Files.readAllLines(Paths.get(dir), Charset.forName("UTF-8"));
+			
+			return "";
+		/*	List<String> lines = Files.readAllLines(Paths.get(dir), Charset.forName("UTF-8"));
 
 			if (lines.size() - 1 < 0) {
 				throw new ResponseException("Fail to recover logs error.");
 			} else {
 				return lines.get(lines.size() - 1);
 
-			}
+			}*/
 		} catch (Exception error) {
 			LOGGER.log(Level.SEVERE, error.getMessage());
 			throw new ResponseException(error);
