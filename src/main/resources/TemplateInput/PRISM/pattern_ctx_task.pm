5c747b16fb89f435b1f05065b2f1a3927a89bc14
Caio Peluti <cpeluti@gmail.com>
2024-06-19 11:51:45 -0300

    Fix: fail line changed to skip

--- src/main/resources/TemplateInput/PRISM/pattern_ctx_task.pm
+++ src/main/resources/TemplateInput/PRISM/pattern_ctx_task.pm
@@ -1,1 +1,1 @@
-[success$PREV_TIME_SLOT$] $PREV_SUCCESS$!$CTX_CONDITION$ s$GID$ = 0 -> (s$GID$'=4);//init to fail
+[success$PREV_TIME_SLOT$] $PREV_SUCCESS$!$CTX_CONDITION$ s$GID$ = 0 -> (s$GID$'=3);//init to skipped
